8fd5440e3550f3668c52d35e68fa4cf910246b37
TB <thrishbg15@gmail.com>
2025-10-01 14:18:30 +0100

    Create server, dotenv, express and set up ES Module

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import {fileURLToPath} from 'url';
+import {dirname} from 'path';
+import path from 'path';
+
+dotenv.config()
+const port = process.env._PORT;
+const API_KEY = process.env.API_KEY
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const staticPath = path.join(__dirname, '/frontend');
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(staticPath, 'index.html'))
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+});
