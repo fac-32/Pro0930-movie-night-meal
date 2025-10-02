8697933a19cd105987e2699fa1bf986a445011f3
Mark O Meara <markomeara98@gmail.com>
2025-10-02 13:03:54 +0100

    [feat] Got basic movie filtering working and showing movies

---
+++ frontend/script.js
@@ -0,0 +1,65 @@
+const submitButton = document.getElementById("filterSubmit");
+const genreSelection = document.getElementById("genre");
+const adultSelection = document.getElementById("isAdult");
+
+
+submitButton.addEventListener("click",  async () => {
+
+    console.log("Submitting choices")
+    const currentGenreID = genreSelection.value
+    const isAdult = adultSelection.checked
+    fetchMovies(currentGenreID, isAdult);
+});
+
+async function fetchMovies(genreID, isAdult) {
+
+    // call API - get movies
+  const url = `/get-movies?genreID=${genreID}&isAdult=${isAdult}`
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+
+    // get first movie
+    const movies = result.results
+    populateMovies(movies)
+
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function populateMovies(movies) {
+
+    const moviesContainer = document.getElementById("moviesContainer");
+    moviesContainer.innerHTML = '';
+
+    movies.forEach(movie => {
+    // For each plant, build a “card” with image + name
+    const card = document.createElement("div");
+    card.classList.add("movie-card");
+
+    const img = document.createElement("img");
+    img.src = `https://media.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`;
+    img.alt = movie.title;
+    card.appendChild(img);
+
+    // Add the name / description
+    const info = document.createElement("div");
+    info.classList.add("movie-info");
+
+    const nameEl = document.createElement("h3");
+    nameEl.textContent = movie.title;
+
+    info.appendChild(nameEl);
+
+    card.appendChild(info);
+    moviesContainer.appendChild(card);
+
+    })
+}
--- server.js
+++ server.js
@@ -7,17 +7,43 @@ import path from 'path';
 dotenv.config()
 const port = process.env._PORT;
 const API_KEY = process.env.API_KEY
+const TMDB_API_KEY = process.env.TMDB_API_KEY
+
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const staticPath = path.join(__dirname, '/frontend');
 
-
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(staticPath, 'index.html'))
 });
 
+// filter movies
+app.get('/get-movies', async (req, res) => {
+
+  try {
+
+    const genreID = req.query.genreID
+    const isAdult = req.query.isAdult
+    const url = `https://api.themoviedb.org/3/discover/movie?include_adult=${isAdult}&language=en-US&page=1&with_genres=${genreID}&api_key=${TMDB_API_KEY}`;
+    console.log(url)
+    console.log("about to call API")
+
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+    res.send(result)
+
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.use(express.static(staticPath))
 
 
 app.listen(port, () => {
