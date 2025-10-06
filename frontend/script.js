e686a1a0ae2668c380b4890f9f55b6eacc3ac0bc
TB <thrishbg15@gmail.com>
2025-10-06 23:49:55 +0100

    feat: add game button and movie filmed location page

--- frontend/script.js
+++ frontend/script.js
@@ -1,110 +1,124 @@
-const submitButton = document.getElementById("filterSubmit");
-const genreSelection = document.getElementById("genre");
-const startYearSelection = document.getElementById("startYear");
-const endYearSelection = document.getElementById("endYear");
-const errorMessage = document.getElementById("error-message");
+window.addEventListener("load", function () {
+  const submitButton = document.getElementById("filterSubmit");
+  const genreSelection = document.getElementById("genre");
+  const startYearSelection = document.getElementById("startYear");
+  const endYearSelection = document.getElementById("endYear");
+  const errorMessage = document.getElementById("error-message");
+  const choiceContainer = document.getElementById("choice-container");
 
-const modal = document.getElementById("myModal");
-const span = document.getElementById("modal-close");
-const modalTitle = document.getElementById("modalMovieTitle");
-const modalPoster = document.getElementById("modalMovieImg");
-const modalOverview = document.getElementById("movieOverview");
+  const modal = document.getElementById("myModal");
+  const span = document.getElementById("modal-close");
+  const modalTitle = document.getElementById("modalMovieTitle");
+  const modalPoster = document.getElementById("modalMovieImg");
+  const modalOverview = document.getElementById("movieOverview");
 
-var validFilterInput = true;
+  var validFilterInput = true;
 
-submitButton.addEventListener("click", async () => {
-  if (!validFilterInput) {
-    return;
-  }
-
-  console.log("Submitting choices");
-  const currentGenreID = genreSelection.value;
-  const startYear = startYearSelection.value;
-  const endYear = endYearSelection.value;
-
-  fetchMovies(currentGenreID, startYear, endYear);
-});
-
-async function fetchMovies(genreID, startYear, endYear) {
-  // get start and end year dates
-  const startDate = new Date(startYear, 0, 1).toISOString().slice(0, 10);
-  const endDate = new Date(endYear, 11, 31).toISOString().slice(0, 10);
-
-  // call API - get movies
-  const url = `/get-movies?genreID=${genreID}&startDate=${startDate}&endDate=${endDate}`;
-
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
+  submitButton.addEventListener("click", async () => {
+    if (!validFilterInput) {
+      return;
     }
 
-    const result = await response.json();
-    console.log(result);
+    console.log("Submitting choices");
+    const currentGenreID = genreSelection.value;
+    const startYear = startYearSelection.value;
+    const endYear = endYearSelection.value;
 
-    const movies = result.results;
-    populateMovies(movies);
-  } catch (error) {
-    console.error(error.message);
-  }
-}
-
-function populateMovies(movies) {
-  const moviesContainer = document.getElementById("moviesContainer");
-  moviesContainer.innerHTML = "";
-
-  movies.forEach((movie) => {
-    const card = document.createElement("button");
-    card.classList.add("movie-card");
-
-    card.addEventListener("click", () => {
-      // populate movie modal
-      modal.style.display = "block";
-      modalTitle.textContent = movie.title;
-      modalPoster.src = `https://media.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`;
-      modalOverview.textContent = movie.overview;
-    });
-
-    const img = document.createElement("img");
-    img.src = `https://media.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`;
-    img.alt = movie.title;
-    card.appendChild(img);
-
-    const info = document.createElement("div");
-    info.classList.add("movie-info");
-
-    const nameEl = document.createElement("h3");
-    nameEl.textContent = movie.title;
-
-    info.appendChild(nameEl);
-
-    card.appendChild(info);
-    moviesContainer.appendChild(card);
+    fetchMovies(currentGenreID, startYear, endYear);
   });
-}
 
-span.onclick = function () {
-  modal.style.display = "none";
-};
+  async function fetchMovies(genreID, startYear, endYear) {
+    // get start and end year dates
+    const startDate = new Date(startYear, 0, 1).toISOString().slice(0, 10);
+    const endDate = new Date(endYear, 11, 31).toISOString().slice(0, 10);
 
-window.onclick = function (event) {
-  if (event.target == modal) {
+    // call API - get movies
+    const url = `/get-movies?genreID=${genreID}&startDate=${startDate}&endDate=${endDate}`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+
+      const movies = result.results;
+      populateMovies(movies);
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  function populateMovies(movies) {
+    const moviesContainer = document.getElementById("moviesContainer");
+    moviesContainer.innerHTML = "";
+
+    movies.forEach((movie) => {
+      const card = document.createElement("button");
+      card.classList.add("movie-card");
+
+      card.addEventListener("click", () => {
+        // populate movie modal
+        modal.style.display = "block";
+        modalTitle.textContent = movie.title;
+        modalPoster.src = `https://media.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`;
+        modalOverview.textContent = movie.overview;
+        localStorage.setItem("filmTitle", movie.title);
+      });
+
+      const img = document.createElement("img");
+      img.src = `https://media.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`;
+      img.alt = movie.title;
+      card.appendChild(img);
+
+      const info = document.createElement("div");
+      info.classList.add("movie-info");
+
+      const nameEl = document.createElement("h3");
+      nameEl.textContent = movie.title;
+
+      info.appendChild(nameEl);
+
+      card.appendChild(info);
+      moviesContainer.appendChild(card);
+    });
+  }
+
+  span.onclick = function () {
     modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
+
+  function validateReleaseYears() {
+    const startYear = parseInt(startYearSelection.value, 10);
+    const endYear = parseInt(endYearSelection.value, 10);
+
+    if (startYear > endYear) {
+      errorMessage.style.display = "inline";
+      validFilterInput = false;
+    } else {
+      errorMessage.style.display = "none";
+      validFilterInput = true;
+    }
   }
-};
 
-function validateReleaseYears() {
-  const startYear = parseInt(startYearSelection.value, 10);
-  const endYear = parseInt(endYearSelection.value, 10);
+  startYearSelection.addEventListener("input", validateReleaseYears);
+  endYearSelection.addEventListener("input", validateReleaseYears);
 
-  if (startYear > endYear) {
-    errorMessage.style.display = "inline";
-    validFilterInput = false;
-  } else {
-    errorMessage.style.display = "none";
-    validFilterInput = true;
-  }
-}
+  const gameBtn = document.createElement("button");
+  gameBtn.classList.add("gameBtn");
+  gameBtn.type = "button";
+  gameBtn.textContent = "Play Game?";
+  choiceContainer.appendChild(gameBtn);
 
-startYearSelection.addEventListener("input", validateReleaseYears);
-endYearSelection.addEventListener("input", validateReleaseYears);
+  gameBtn.addEventListener("click", () => {
+    window.location.href = "filmLocation/filmLocation.html";
+  });
+});
